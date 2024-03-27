9a59bd7a4fa53048b9aa012d8e2cfdf49fc14707
Gyulo94 <qkrcjfrb112@gmail.com>
2024-03-27 22:21:14 +0900

    푸터 추가

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <p>@Copyright by 찰규 </p>
+    </footer>
+  );
+};
