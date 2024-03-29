027f51572805dde0b9ee08134cc3ef1aa5707696
Gyulo94 <qkrcjfrb112@gmail.com>
2024-03-29 09:18:16 +0900

    각 페이지별 라우팅 추가 home, about, blog, portfolio, contact, signin, signup, not found

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import About from "./pages/About/About";
+import Blog from "./pages/Blog/Blog";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </div>
   );
